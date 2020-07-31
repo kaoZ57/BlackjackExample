5278589cbdcbffe07f74b7ce050db8f216aede70
EkkalakChimjan <ch.ekkalak@gmail.com>
2020-07-31 11:14:22 +0700

    update description text

--- EkkalakChimjan.BlackjackExample/Hand.cs
+++ EkkalakChimjan.BlackjackExample/Hand.cs
@@ -7,14 +7,13 @@ namespace EkkalakChimjan.BlackjackExample
     internal class Hand
     {
         private Player player;
-        public string line = "------------------------------";
         public List<Card> CardList { get; private set; }
 
         public int Bet { get; set; }
         public string Name { get; }
         public bool Stay { get; set; }
 
-        public Hand(Player player,string name)
+        public Hand(Player player, string name)
         {
             this.player = player;
             Stay = false;
@@ -22,6 +21,7 @@ namespace EkkalakChimjan.BlackjackExample
             Bet = 0;
             CardList = new List<Card>();
         }
+
         public bool SetBet(int money)
         {
             if (player.Money < money || money < 1)
@@ -32,23 +32,37 @@ namespace EkkalakChimjan.BlackjackExample
             player.AddMoney(money * -1);
             return true;
         }
+
         public void AddCard(Card card)
         {
             CardList.Add(card);
         }
+
         public int NumberOfCards => CardList.Count;
 
         public delegate void Logic(Hand hand);
+
+        static public void printStayText(string playerName, string handName)
+        {
+            Console.WriteLine("The {0}'s hand \"{0}\" stay !", playerName, handName);
+        }
+
+        static public void printNotStayText(string playerName, string handName)
+        {
+            Console.WriteLine("The {0}'s hand \"{1}\" want one more cards !", playerName, handName);
+        }
 
         public static void AILogic(Hand hand)
         {
             if (hand.Point >= 17)
             {
                 hand.Stay = true;
+                Hand.printStayText(hand.player.Name, hand.Name);
             }
             else
             {
                 hand.Stay = false;
+                Hand.printNotStayText(hand.player.Name, hand.Name);
             }
         }
 
@@ -57,7 +71,14 @@ namespace EkkalakChimjan.BlackjackExample
             Console.WriteLine(hand.ToString());
             Console.Write("  Do you want to \"stay\" for hand \"{0}\"? (y/n): ", hand.Name);
             hand.Stay = Console.ReadLine().ToLower() == "y";
-            Console.WriteLine(hand.line);
+            if (hand.Stay)
+            {
+                Hand.printStayText(hand.player.Name, hand.Name);
+            }
+            else
+            {
+                Hand.printNotStayText(hand.player.Name, hand.Name);
+            }
         }
 
         public bool isStay(Logic logic)
@@ -68,10 +89,28 @@ namespace EkkalakChimjan.BlackjackExample
 
         public override string ToString()
         {
-            string returnText = string.Format("{0}\n Cards in {1}'s hand \"{2}\" (Bet: {3})\n",line,player.Name, Name,Bet);
-            CardList.ForEach(card => returnText += string.Format("  {0}\n", card.ToString()));
-            returnText += string.Format("  Total : {0} Points", Point);
+            string line = "------------------------------";
+            string returnText = string.Format("{0}\n Cards in {1}'s hand \"{2}\" (Bet: {3})\n", line, player.Name, Name, Bet);
+            CardList.ForEach(card => returnText += string.Format("  {0},  ", card.ToString()));
+            returnText += string.Format("\n  Total : {0} Points", Point);
             return returnText;
+        }
+
+        public void ShowOneCard()
+        {
+            string text = "------------------------------\n";
+            text += string.Format("Cards in {0}'s hand \"{1}\"\n", player.Name, Name);
+            for (int i = 0; i < CardList.Count; i++)
+            {
+                if (i == 0)
+                {
+                    text += string.Format("  {0}\n", CardList[i].ToString());
+                }
+                else
+                {
+                    text += "  [?],  \n";
+                }
+            }
         }
 
         public int Point
